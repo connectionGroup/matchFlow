5037f74cd5c7b72329ec979cddd2e2ef5523e1f3
Veronica Martinez Cadavid <veromar.c96@gmail.com>
2026-01-29 20:25:14 -0500

    feat: company profile

---
+++ js/utils.js
@@ -0,0 +1,6 @@
+export const company = {
+    name:'The Company',
+    industry:'The Industry',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. At mollitia tempora numquam dolorem deleniti qui distinctio temporibus voluptatum veniam quidem adipisci ducimus exercitationem ea expedita, quas repudiandae sit, nesciunt eum.',
+    jobs:[],
+}
