79d45c16035a369707a5c4223d9ec937bedc8194
Choklitos21 <diego.mm09@hotmail.com>
2026-01-29 20:20:36 -0500

    feat: register on work

---
+++ js/register.js
@@ -0,0 +1,35 @@
+import * as storage from "./storage"
+
+document.addEventListener("DOMContentLoaded", () => {
+    let loggedUser = JSON.parse(sessionStorage.getItem("loggedUser"))
+    if (loggedUser) {
+        if (loggedUser.rol === "candidate") {
+            window.location.href = "./../pages/candidateHome"
+        } else {
+            window.location.href = "./../pages/companyHome"
+        }
+    }
+})
+
+document.getElementById("registerForm").addEventListener("submit", async (e) => {
+    e.preventDefault()
+
+    let formData = new FormData(e.target)
+    let data = {}
+    formData.forEach((value, key) => {
+        console.log(`data[${key}] = ${value}`)
+        data[key] = value
+    })
+
+    let userFound = await storage.verifyUser(data)
+
+    if (userFound) {
+        const miModal = new bootstrap.Modal(document.getElementById('modalRegistro'));
+        miModal.show();
+        return false
+    }
+
+    await storage.saveUser({...data, status: true})
+
+
+})
---
+++ js/storage.js
@@ -0,0 +1,54 @@
+export async function verifyUser(user) {
+    try {
+        const response = await fetch('http://localhost:4001/users'); // Await the response
+        const data = await response.json(); // Await the JSON parsing
+        console.log(data);
+
+        for (let i = 0; i < data.length; i++) {
+            if (data[i].email === user.email) {
+                return true
+            }
+        }
+        return false
+    } catch (error) {
+        console.error('There was a problem with the fetch operation:', error);
+    }
+}
+
+export async function saveUser(user) {
+    try {
+        const response = await fetch('http://localhost:3000/users', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        });
+
+        const responseData = await response.json();
+        console.log('Success:', responseData);
+        return responseData;
+
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+export async function saveCompany(company) {
+    try {
+        const response = await fetch('http://localhost:3000/companies', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(company)
+        });
+
+        const responseData = await response.json();
+        console.log('Success:', responseData);
+        return responseData;
+
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
