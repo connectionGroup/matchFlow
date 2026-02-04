232e31bb6cb9d6c5213c68d23f37c4ff3888dd4d
kevincano218-eng <kevincano218@gmail.com>
2026-02-04 13:52:26 -0500

    modifications

--- js/login.js
+++ js/login.js
@@ -1,38 +1,38 @@
-let user_email = document.getElementById('user');
-let user_password = document.getElementById('password');
-let button_link = document.getElementById('button');
-let rol_user = document.getElementById('rol');
+import * as storage from "./storage.js"
 
-
-button_link.addEventListener('click', function (event){
-    event.preventDefault();
-
-    fetch(`http://localhost:4001/users?email=${user_email.value}&password=${user_password.value}`)
-    .then(response => response.json())
-        .then(data => {
-            if (data.length > 0) {
-                const user = data[0];
-
-                if (user.role === role_user.value) {
-                    sessionStorage.setItem('session', 'yes');
-                    sessionStorage.setItem('role', user.role);
-
-                    if (user.role === "Administrador") {
-                        window.location.href = "./admin.html";
-                    } else {
-                        window.location.href = "./usuario.html";
-                    }
-                } else {
-                    alert("El rol seleccionado no coincide");
-                }
-            } else {
-                alert("Usuario o contraseña incorrectos");
-            }
-        })
-        .catch(error => {
-            console.error("Error:", error);
-            alert("Error al conectar con el servidor");
-        });
+document.addEventListener("DOMContentLoaded", () => {
+    let loggedUser = JSON.parse(sessionStorage.getItem("loggedUser"))
+    if (loggedUser) {
+        if (loggedUser.role === "candidate") {
+            window.location.replace("./../pages/candidateHome.html")
+        } else {
+            window.location.replace("./../pages/companyHome.html")
+        }
+    }
 })
 
+document.getElementById("loginForm").addEventListener("submit", async (e) => {
+    e.preventDefault()
 
+    let formData = new FormData(e.target)
+    let data = {}
+    formData.forEach((value, key) => {
+        console.log(`data[${key}] = ${value}`)
+        data[key] = value
+    })
+
+    const userFound = await storage.verifyUser(data)
+
+    if(!userFound) {
+        const miModal = new bootstrap.Modal(document.getElementById('modalRegistro'));
+        miModal.show();
+        return false
+    }
+
+    sessionStorage.setItem("loggedUser", JSON.stringify({email: userFound.email, role: userFound.role}))
+    if (userFound.role === "candidate") {
+        window.location.replace("./../pages/perfil.html")
+    } else {
+        window.location.replace("./../pages/companyHome.html")
+    }
+})
---
+++ js/storage.js
@@ -0,0 +1,35 @@
+export async function verifyUser(user) {
+    try {
+        const response = await fetch('http://localhost:4000/users'); // Await the response
+        const data = await response.json(); // Await the JSON parsing
+        console.log(data);
+
+        for (let i = 0; i < data.length; i++) {
+            if (data[i].email === user.email) {
+                return data[i]
+            }
+        }
+        return false
+    } catch (error) {
+        console.error('There was a problem with the fetch operation:', error);
+    }
+}
+
+export async function saveUser(user) {
+    try {
+        const response = await fetch('http://localhost:4000/users', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        });
+
+        const responseData = await response.json();
+        console.log('Success:', responseData);
+        return responseData;
+
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
